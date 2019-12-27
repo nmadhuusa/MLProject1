17ed4fe90c039dc42378979553da3660fa14beb3
nmadhuusa <37255680+nmadhuusa@users.noreply.github.com>
2019-12-26 20:37:15 -0500

    Create ed01.py

---
+++ ed01.py
@@ -0,0 +1,12 @@
+import urllib.request, urllib.parse, urllib.error
+import re, zipfile, glob, os
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+#urlName = 'https://www.sec.gov/cgi-bin/srch-edgar?text=N-CEN'
+#urllib.request.urlretrieve(urlName, "aaa.html")
+
+
+
